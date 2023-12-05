1267c9c1e7f130926c2b205efb59c34c8d338874
Saqib <prosaqib2022@gmail.com>
2023-12-05 11:28:27 +0500

    Imaginary Friends

---
+++ Code Challenges 1/Imaginary Friends.js
@@ -0,0 +1,9 @@
+
+// imaginaryFriends ()
+
+function numImaginaryFriends(totalFriends){
+    return Math.ceil(totalFriends * .25)
+    }
+    console.log(numImaginaryFriends(20)) // Should print 5
+    console.log(numImaginaryFriends(10)) // Should print 3 (2.5 rounded up!)
+  
