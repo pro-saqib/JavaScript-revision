64b5314bf1fff10d10efd83e4ee4cd69268a104e
Saqib <prosaqib2022@gmail.com>
2023-12-05 11:29:10 +0500

    Silly Sentence

--- Code Challenges 1/Code Challenges.js
+++ Code Challenges 1/Code Challenges.js
@@ -1,12 +1,6 @@
 
 
 
-//  sillySentence()
-function sillySentence(firstWord,secondWord,thirdWord){
-  return console.log(`I am so ${firstWord} because I ${secondWord}! Time to write some more awesome ${thirdWord}!`)
-}
-console.log(sillySentence('excited', 'love', 'functions')) 
-
 // colorMessage
 
 function colorMessage(favoriteColor,shirtColor){
---
+++ Code Challenges 1/Silly Sentence.js
@@ -0,0 +1,7 @@
+
+//  sillySentence()
+function sillySentence(firstWord,secondWord,thirdWord){
+    return console.log(`I am so ${firstWord} because I ${secondWord}! Time to write some more awesome ${thirdWord}!`)
+  }
+  console.log(sillySentence('excited', 'love', 'functions')) 
+  
