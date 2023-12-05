5353ab19831d6b31c9bee8987b265493f399eb0b
Saqib <prosaqib2022@gmail.com>
2023-12-05 11:30:09 +0500

    Color Message

--- Code Challenges 1/Code Challenges.js
+++ Code Challenges 1/Code Challenges.js
@@ -1,15 +1,6 @@
 
 
 
-// colorMessage
-
-function colorMessage(favoriteColor,shirtColor){
-  (favoriteColor===shirtColor) ? 
-  console.log('The shirt is your favorite color!') : console.log('That is a nice color.')
-  }
-  colorMessage('red','red')
-  colorMessage('red','green')
-
 // isEven()
 
 const isEven = x => {
---
+++ Code Challenges 1/Color Message.js
@@ -0,0 +1,10 @@
+
+// colorMessage
+
+function colorMessage(favoriteColor,shirtColor){
+    (favoriteColor===shirtColor) ? 
+    console.log('The shirt is your favorite color!') : console.log('That is a nice color.')
+    }
+    colorMessage('red','red')
+    colorMessage('red','green')
+  
