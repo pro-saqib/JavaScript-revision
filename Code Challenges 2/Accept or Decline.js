50e383398e2b4b4c1e36c3b8c9850b617b943178
Saqib <prosaqib2022@gmail.com>
2023-12-06 22:31:17 +0500

    Accept or Decline

---
+++ Code Challenges 2/Accept or Decline.js
@@ -0,0 +1,20 @@
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = veg => {
+      console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+}
+
+// Write your code here:
+
+function declineEverything(arr){
+  arr.forEach(politelyDecline)
+}
+declineEverything(veggies)
+
+function acceptEverything(arr){
+  arr.forEach((e=>{
+    console.log('Ok, I guess I will eat some ' + e + '.')
+  }))
+}
+
+acceptEverything(veggies)
