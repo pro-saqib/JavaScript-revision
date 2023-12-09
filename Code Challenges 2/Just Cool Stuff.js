c0acdb9ffd3fcf73fb38a77f8297b1fd608d5104
Saqib <prosaqib2022@gmail.com>
2023-12-09 22:30:15 +0500

    Just Cool Stuff 2

--- Code Challenges 2/Just Cool Stuff.js
+++ Code Challenges 2/Just Cool Stuff.js
@@ -2,16 +2,16 @@ const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => sec
 
 /*
 // As a function declaration AND using named function w/ a loop:
-function justCoolStuff(firstArray, secondArray) {
-      function isInSecondArray(item){
-            for(let i = 0; i<secondArray.length; i++){
-                  if (secondArray[i] === item){
-                        return true
-                  }
-            }
-            return false 
+function justCoolStuff(arr1,arr2){
+  checks = item => {
+    for(let i=0; i<=arr2.length; i++){
+      if(arr2[i]===item){
+        return true
       }
-      return firstArray.filter(isInSecondArray)
+    }
+    return false
+  }
+  return arr1.filter(checks)
 }
 */
 
